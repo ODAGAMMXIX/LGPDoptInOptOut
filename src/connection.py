91d477f3069d2287bcd1bde72b20a3a3fc134ad3
ODAGAMMXIX <fabio.odaguiri.alt.email@gmail.com>
2022-04-27 00:44:36 -0300

    Z

--- src/connection.py
+++ src/connection.py
@@ -4,4 +4,3 @@ def create_connection():
     application_client = pymongo.MongoClient("mongodb://localhost:27017/")
     application_db = application_client["OptInOptOut"]
     return application_db
-#
